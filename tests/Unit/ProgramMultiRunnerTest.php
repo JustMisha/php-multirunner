2b56fff8be584cc313cd27aed7d9f72216be8d0e
Mikhail Trusov <admin@superprogrammist.ru>
2024-12-31 18:02:14 +0300

    ProgramMultiRunnerTest class refactored

--- tests/Unit/ProgramMultiRunnerTest.php
+++ tests/Unit/ProgramMultiRunnerTest.php
@@ -1,16 +1,28 @@
 <?php
 
-namespace JustMisha\MultiRunner\Tests\Unit;
+/**
+ * MultiRunner test classes: ProgramMultiRunnerTest class.
+ *
+ * @package JustMisha\MultiRunner
+ * @license https://github.com/JustMisha/php-multirunner/LICENSE.md MIT License
+ */
 
+namespace JustMisha\MultiRunner\Tests\Unit;
 
 use JustMisha\MultiRunner\DTO\ProcessResults;
 use JustMisha\MultiRunner\ProgramMultiRunner;
 use JustMisha\MultiRunner\Tests\BaseTestCase;
 
-
+/**
+ * Tests multiple running several instances of a program simultaneously.
+ */
 class ProgramMultiRunnerTest extends BaseTestCase
 {
-
+    /**
+     *
+     *
+     * @return void
+     */
     public function testStandardRun(): void
     {
         $runner = new ProgramMultiRunner(
@@ -20,11 +32,12 @@ class ProgramMultiRunnerTest extends BaseTestCase
             ($this->isWindows() ? 'helloworld_win64.exe' : 'helloworld'),
             [],
             null,
-            null);
+            null
+        );
 
         $totalProcessNums = 5;
-        for($i = 1; $i <= $totalProcessNums; $i++) {
-            $runner->addProcess((string)$i);
+        for ($i = 1; $i <= $totalProcessNums; $i++) {
+            $runner->addProcess('string' . $i);
         }
 
         $timeout = 5;
@@ -34,26 +47,27 @@ class ProgramMultiRunnerTest extends BaseTestCase
         $expectedResult = new ProcessResults(0, "Hello world!", "");
 
         $this->assertCount(($totalProcessNums), $results);
-        $this->assertEquals($expectedResult, $results[1]);
-        $this->assertEquals($expectedResult, $results[$totalProcessNums]);
+        $this->assertEquals($expectedResult, $results['string' . 1]);
+        $this->assertEquals($expectedResult, $results['string' . $totalProcessNums]);
 
         unset($runner);
-
     }
 
     public function testRunWhenDelay(): void
     {
-        $runner = new ProgramMultiRunner(self::MAX_PARALLEL_PROCESSES,
+        $runner = new ProgramMultiRunner(
+            self::MAX_PARALLEL_PROCESSES,
             dirname(__FILE__, 2) . DIRECTORY_SEPARATOR .
             'fixtures' . DIRECTORY_SEPARATOR .
             ($this->isWindows() ? 'sleep_3_and_optput_helloworld_win64.exe' : 'sleep_3_and_output_helloworld'),
             [],
             null,
-            null);
+            null
+        );
 
         $totalProcessNums = 5;
-        for($i = 1; $i <= $totalProcessNums; $i++) {
-            $runner->addProcess((string)$i);
+        for ($i = 1; $i <= $totalProcessNums; $i++) {
+            $runner->addProcess('string' . $i);
         }
 
         $timeout = 5;
@@ -67,15 +81,17 @@ class ProgramMultiRunnerTest extends BaseTestCase
         $expectedResult = new ProcessResults(0, "Hello world!", "");
 
         $this->assertCount($totalProcessNums, $results);
-        $this->assertEquals($expectedResult, $results[1]);
-        $this->assertEquals($expectedResult, $results[$totalProcessNums]);
+        $this->assertEquals($expectedResult, $results['string' . 1]);
+        $this->assertEquals($expectedResult, $results['string' . $totalProcessNums]);
 
         unset($runner);
-
     }
 
-    //
-
+    /**
+     *
+     *
+     * @return void
+     */
     public function testRunWithBadArgument(): void
     {
         $runner = new ProgramMultiRunner(
@@ -91,8 +107,8 @@ class ProgramMultiRunnerTest extends BaseTestCase
         $badArgument = 'String with "C:\\quotes\\" or malicious %OS% (&()[]{}^=;!\'+,`~) stuff \\';
         $badArgument .= '&()[]{}^=;!\'+,`~';
         $totalProcessNums = 5;
-        for($i = 1; $i <= $totalProcessNums; $i++) {
-            $runner->addProcess((string)$i, $badArgument);
+        for ($i = 1; $i <= $totalProcessNums; $i++) {
+            $runner->addProcess('string' . $i, $badArgument);
         }
 
         $timeout = 5;
@@ -102,11 +118,10 @@ class ProgramMultiRunnerTest extends BaseTestCase
         $expectedResult = new ProcessResults(0, "Hello world!", "");
 
         $this->assertCount(($totalProcessNums), $results);
-        $this->assertEquals($expectedResult, $results[1]);
-        $this->assertEquals($expectedResult, $results[$totalProcessNums]);
+        $this->assertEquals($expectedResult, $results['string' . 1]);
+        $this->assertEquals($expectedResult, $results['string' . $totalProcessNums]);
 
         unset($runner);
-
     }
 
     public function testRunWithCWD(): void
@@ -114,12 +129,14 @@ class ProgramMultiRunnerTest extends BaseTestCase
         $runner = new ProgramMultiRunner(
             self::MAX_PARALLEL_PROCESSES,
             ($this->isWindows() ? 'helloworld_win64.exe' : 'helloworld'),
-            [], dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR,
-            null);
+            [],
+            dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR,
+            null
+        );
 
         $totalProcessNums = 5;
-        for($i = 1; $i <= $totalProcessNums; $i++) {
-            $runner->addProcess((string)$i);
+        for ($i = 1; $i <= $totalProcessNums; $i++) {
+            $runner->addProcess('string' . $i);
         }
 
         $timeout = 5;
@@ -129,10 +146,9 @@ class ProgramMultiRunnerTest extends BaseTestCase
         $expectedResult = new ProcessResults(0, "Hello world!", "");
 
         $this->assertCount(($totalProcessNums), $results);
-        $this->assertEquals($expectedResult, $results[1]);
-        $this->assertEquals($expectedResult, $results[$totalProcessNums]);
+        $this->assertEquals($expectedResult, $results['string' . 1]);
+        $this->assertEquals($expectedResult, $results['string' . $totalProcessNums]);
 
         unset($runner);
-
     }
 }
